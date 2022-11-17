553528a32542a1018e4eefb9029f4cde7e25dfdd
Igor Rakhmanov <rahmanov.math@gmail.com>
2022-11-17 13:36:27 +0100

    changes in mcm group names

--- Settings/MCMSettingsGroupNames.cs
+++ Settings/MCMSettingsGroupNames.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Bannerlord.XPTweaks.Settings
+﻿namespace Bannerlord.XPTweaks.Settings
 {
     internal static class MCMSettingsGroupNames
     {
-        //public const string Smithing = "{=I34ardNfzvnvE}Smithing";
-        //public const string SmithingRarityChance = "{=WYDp2KtTRIWT7}Smithing/Rarity chance";
-        //public const string SmithingRarityBonus = "{=fiEcFzWSLZoVC}Smithing/Rarity bonus";
+        public const string Smithing = "{=I34ardNfzvnvE}Smithing";
+        public const string SmithingRarityChance = Smithing + "/{=WYDp2KtTRIWT7}Rarity chance";
+        public const string SmithingRarityBonus = Smithing + "/{=fiEcFzWSLZoVC}Rarity bonus";
 
-        //public const string ExperienceFactors = "{=x63OTfGg3hkHt}Experience/Factors";
-        //public const string ExperienceLearning = "{=mV7FaNIGlMRSD}Experience/Learning rates and limits";
-        //public const string ExperienceTradeAdditionalXp = "{=5B5MjvBIQlO5B}Experience/Trade/Additional Trade XP from any deal";
+        public const string Experience = "{=F4KbLwGtGKFv7}Experience";
+        public const string ExperienceFactors = Experience + "/{=x63OTfGg3hkHt}Factors";
+        public const string ExperienceLearning = Experience + "/{=mV7FaNIGlMRSD}Learning rates and limits";
+        public const string ExperienceTrade = Experience + "/{=IBQCzbLjZy7pH}Trade";
+        public const string ExperienceTradeAdditionalXp = ExperienceTrade + "/{=5B5MjvBIQlO5B}Additional Trade XP from any deal";
 
-        //public const string CharacterDevelopment = "{=BX1VpXfkokzkE}Character Development";
+        public const string CharacterDevelopment = "{=BX1VpXfkokzkE}Character Development";
 
-        public const string Smithing = "Smithing";
-        public const string SmithingRarityChance = "Smithing/Rarity chance";
-        public const string SmithingRarityBonus = "Smithing/Rarity bonus";
+        public const string Renown = "{=ZhEzdj52VaeN6}Renown";
 
-        public const string ExperienceFactors = "Experience";
-        public const string ExperienceLearning = "Experience";
-        public const string ExperienceTradeAdditionalXp = "Experience/Trade";
-
-        public const string CharacterDevelopment = "Character Development";
-
-        public const string Renown = "Renown";
-
-        public const string Party = "Party";
+        public const string Party = "{=E7i88SQTFWjSJ}Party";
     }
 }
